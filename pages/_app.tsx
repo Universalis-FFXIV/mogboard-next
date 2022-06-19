064fd115d577c48f1ca5928c833d4084474e6caf
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-18 17:49:25 -0700

    Add assets and head tag data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/app.scss';
+import '../styles/font/styles.css';
+import '../styles/font/xivicons.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const MogboardDocument = () => {
+  return (
+    <Html>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://universalis.app" />
+        <meta property="og:image" content="https://universalis.app/i/universalis/universalis.png" />
+        <meta name="keywords" content="FFXIV,Market,Boards,Database" />
+        <meta
+          name="google-site-verification"
+          content="sAL0Wyt9UZG63TXf1FK2aqMVOiGhcCqjZBuIp2onG0s"
+        />
+        <meta name="theme-color" content="#BD983A" />
+
+        <link rel="alternate" type="application/json+oembed" href="/json/oembed.json" />
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" type="image/png" href="/i/universalis/universalis_ios.png" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Cinzel|Fjalla+One&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default MogboardDocument;
