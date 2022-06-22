787b00a2f3e39f70bb6aaee4ad4cd8ccc0649948
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-21 20:32:30 -0700

    Home lists work

--- components/HomeNavbar/HomeNavBar.tsx
+++ components/HomeNavbar/HomeNavBar.tsx
@@ -1,13 +1,29 @@
+import LoggedIn from '../LoggedIn/LoggedIn';
+import LoggedOut from '../LoggedOut/LoggedOut';
+
 export default function HomeNavbar() {
   return (
     <div className="home-nav">
-      <section className="tac">
-        <strong>Logged-out</strong>
-      </section>
-      <p className="text-gray">
-        Lists, Alerts, Market activity and retainer links will show here when you are logged into
-        the site.
-      </p>
+      <LoggedOut>
+        <section className="tac">
+          <strong>Logged-out</strong>
+        </section>
+        <p className="text-gray">
+          Lists, Alerts, Market activity and retainer links will show here when you are logged into
+          the site.
+        </p>
+      </LoggedOut>
+      <LoggedIn>
+        <h3>
+          <img src="/i/svg/th-list-light.svg" alt="" />
+          Lists
+        </h3>
+        <div>
+          <button type="button" className="btn-disabled">
+            <span className="text-gray">You have no lists.</span>
+          </button>
+        </div>
+      </LoggedIn>
     </div>
   );
 }
