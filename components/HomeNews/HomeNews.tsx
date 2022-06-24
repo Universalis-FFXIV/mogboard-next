5870f0d9a8a0239bfef42b81d9723def2146e82e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-23 23:22:47 -0700

    Translate welcome panel

--- components/HomeNews/HomeNews.tsx
+++ components/HomeNews/HomeNews.tsx
@@ -1,18 +1,27 @@
+import { Trans } from '@lingui/macro';
 import Link from 'next/link';
 
 export default function HomeNews() {
   return (
     <div className="the-big-news-post">
-      <h4>Welcome to Universalis!</h4>
+      <h4>
+        <Trans>Welcome to Universalis!</Trans>
+      </h4>
       <p>
-        Universalis is a market board data site with crowd sourced information, based on mogboard.
-        It can aggregate market board information from multiple sources, so if you want to help out,
-        please check out our contributing page.
+        <Trans>
+          Universalis is a market board data site with crowd sourced information, based on mogboard.
+          It can aggregate market board information from multiple sources, so if you want to help
+          out, please check out our contributing page.
+        </Trans>
       </p>
-      <p>Thank you, and enjoy your stay!</p>
+      <p>
+        <Trans>Thank you, and enjoy your stay!</Trans>
+      </p>
 
       <Link href="/contribute">
-        <a>Contribute to market board data</a>
+        <a>
+          <Trans>Contribute to market board data</Trans>
+        </a>
       </Link>
     </div>
   );
