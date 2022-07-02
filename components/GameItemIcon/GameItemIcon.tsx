f9df3e6dc826b3e77f5351a9c52597bf0c68b8d2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-07-01 20:06:44 -0700

    Fix market item icon not changing between pages

--- components/GameItemIcon/GameItemIcon.tsx
+++ components/GameItemIcon/GameItemIcon.tsx
@@ -1,16 +1,27 @@
-import Image from 'next/image';
-import { useState } from 'react';
+import Image, { ImageLoader } from 'next/image';
+import { useEffect, useState } from 'react';
 
 interface GameItemIconProps {
   id: number;
   width: number;
   height: number;
   className?: string;
+  priority?: boolean;
 }
 
-export default function GameItemIcon({ id, width, height, className }: GameItemIconProps) {
+export default function GameItemIcon({
+  id,
+  width,
+  height,
+  className,
+  priority,
+}: GameItemIconProps) {
   const [url, setUrl] = useState(
     `https://universalis-ffxiv.github.io/universalis-assets/icon2x/${id}.png`
+  );
+  useEffect(
+    () => setUrl(`https://universalis-ffxiv.github.io/universalis-assets/icon2x/${id}.png`),
+    [id]
   );
   return (
     <div className={className}>
@@ -22,6 +33,7 @@ export default function GameItemIcon({ id, width, height, className }: GameItemI
         onError={() => {
           setUrl('/i/universalis/error.png');
         }}
+        priority={priority}
         className={className}
       />
     </div>
