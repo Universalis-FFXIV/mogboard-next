ed11d7dcb30ca174e8c95620990529502dc687c4
Kara <49822414+karashiiro@users.noreply.github.com>
2022-10-30 22:35:53 -0700

    Use temp domain (#89)

--- service/universalis.ts
+++ service/universalis.ts
@@ -1,3 +1,3 @@
 export function getBaseUrl(): string {
-  return process.env['NEXT_PUBLIC_API_URL'] || 'https://universalis.app/api';
+  return process.env['NEXT_PUBLIC_API_URL'] || 'https://mogboard.universalis.app/api';
 }
