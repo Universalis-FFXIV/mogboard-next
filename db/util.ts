427d394ebc4a1703a1e7018a0c67418585fc0a56
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-02 15:01:20 -0800

    Add static database interface This database interface includes wrappers for taking and returning connection pool connections. The pool is already a singleton, so it doesn't yet make sense to refactor this more.

--- db/util.ts
+++ db/util.ts
@@ -1,3 +1,19 @@
+import mariadb from 'mariadb';
+import { acquireConn, releaseConn } from './connect';
+
 export function unix() {
   return Math.floor(new Date().valueOf() / 1000);
+}
+
+export async function rentConnectionForFn<T>(
+  fn: (conn: mariadb.Connection) => Promise<T>
+): Promise<T> {
+  const conn = await acquireConn();
+  try {
+    return await fn(conn);
+  } catch (err) {
+    throw err;
+  } finally {
+    await releaseConn(conn);
+  }
 }
