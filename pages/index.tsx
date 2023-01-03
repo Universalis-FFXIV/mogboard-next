9681c49474c130899d6fbfd433dbff2e1066cca1
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-02 20:39:25 -0800

    Use client-side data fetching for market API on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,16 +25,12 @@ import { TaxRates } from '../types/universalis/TaxRates';
 import { unstable_getServerSession } from 'next-auth';
 import HomeLeastRecentlyUpdated from '../components/Home/HomeLeastRecentlyUpdated/HomeLeastRecentlyUpdated';
 import { getBaseUrl } from '../service/universalis';
+import useSWR from 'swr';
 
 interface HomeProps {
   dcs: DataCenter[];
   world: string;
   server: string;
-  taxes: Record<City, number>;
-  recent: number[];
-  leastRecents: { id: number; date: number; world: string }[];
-  dailyUploads: number[];
-  worldUploads: { world: string; count: number }[];
   hasSession: boolean;
   lists: UserList[];
 }
@@ -43,22 +39,86 @@ function sum(arr: number[], start: number, end: number) {
   return arr.slice(start, end).reduce((a, b) => a + b, 0);
 }
 
-const Home: NextPage<HomeProps> = ({
-  dcs,
-  world,
-  server,
-  taxes,
-  recent,
-  leastRecents,
-  dailyUploads,
-  worldUploads,
-  hasSession,
-  lists,
-}: HomeProps) => {
+function convertTaxRates(taxRates: TaxRates): Record<City, number> {
+  return {
+    [City.LimsaLominsa]: taxRates['Limsa Lominsa'] ?? 0,
+    [City.Gridania]: taxRates['Gridania'] ?? 0,
+    [City.Uldah]: taxRates["Ul'dah"] ?? 0,
+    [City.Ishgard]: taxRates['Ishgard'] ?? 0,
+    [City.Kugane]: taxRates['Kugane'] ?? 0,
+    [City.Crystarium]: taxRates['Crystarium'] ?? 0,
+    [City.OldSharlayan]: taxRates['Old Sharlayan'] ?? 0,
+  };
+}
+
+function zeroTaxRates(): Record<City, number> {
+  return {
+    [City.LimsaLominsa]: 0,
+    [City.Gridania]: 0,
+    [City.Uldah]: 0,
+    [City.Ishgard]: 0,
+    [City.Kugane]: 0,
+    [City.Crystarium]: 0,
+    [City.OldSharlayan]: 0,
+  };
+}
+
+const Home: NextPage<HomeProps> = ({ dcs, world, server, hasSession, lists }: HomeProps) => {
   const [settings] = useSettings();
   const lang = settings['mogboard_language'] || 'en';
 
   const [selectedList, setSelectedList] = useState<UserList | undefined>();
+
+  const { data: taxes } = useSWR<Record<City, number>>(
+    `${getBaseUrl()}/tax-rates?world=${world}`,
+    (url) =>
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => convertTaxRates(res))
+  );
+
+  const { data: dailyUploads } = useSWR<number[]>(
+    `${getBaseUrl()}/extra/stats/upload-history`,
+    (url) =>
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => res.uploadCountByDay)
+  );
+
+  const { data: recent } = useSWR<number[]>(`${getBaseUrl()}/extra/stats/recently-updated`, (url) =>
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => res.items.slice(0, 6))
+  );
+
+  const { data: worldUploads } = useSWR<{ world: string; count: number }[]>(
+    `${getBaseUrl()}/extra/stats/world-upload-counts`,
+    (url) =>
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) =>
+          Object.keys(res).map((k) => ({
+            world: k,
+            count: res[k].count,
+          }))
+        )
+  );
+
+  const { data: leastRecents } = useSWR<{ id: number; date: number; world: string }[]>(
+    `${getBaseUrl()}/extra/stats/least-recently-updated?${
+      server.toLowerCase() === world.toLowerCase() ? 'world' : 'dcName'
+    }=${server}`,
+    (url) =>
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) =>
+          res.items.slice(0, 20).map((entry: { [x: string]: string | number }) => ({
+            id: entry.itemID,
+            date: entry.lastUploadTime,
+            world: entry.worldName,
+          }))
+        )
+  );
 
   const title = 'Universalis';
   const description =
@@ -96,7 +156,10 @@ const Home: NextPage<HomeProps> = ({
             server={server}
             multiWorld={settings['mogboard_homeworld'] !== 'yes'}
             lang={lang}
-            leastRecents={leastRecents.map((entry) => ({ ...entry, date: new Date(entry.date) }))}
+            leastRecents={(leastRecents ?? []).map((entry) => ({
+              ...entry,
+              date: new Date(entry.date),
+            }))}
           />
         </div>
         <div>
@@ -104,10 +167,13 @@ const Home: NextPage<HomeProps> = ({
           <h4>
             <Trans>Recent Updates</Trans>
           </h4>
-          <RecentUpdatesPanel items={recent.map((itemId) => getItem(itemId, lang)!)} />
-          <TaxRatesPanel data={taxes} world={world} />
-          <WorldUploadCountsPanel data={worldUploads} world={world} />
-          <UploadCountPanel today={sum(dailyUploads, 0, 1)} week={sum(dailyUploads, 0, 7)} />
+          <RecentUpdatesPanel items={(recent ?? []).map((itemId) => getItem(itemId, lang)!)} />
+          <TaxRatesPanel data={taxes ?? zeroTaxRates()} world={world} />
+          <WorldUploadCountsPanel data={worldUploads ?? []} world={world} />
+          <UploadCountPanel
+            today={sum(dailyUploads ?? [], 0, 1)}
+            week={sum(dailyUploads ?? [], 0, 7)}
+          />
           <p className="mog-honorable" style={{ textAlign: 'center', marginTop: 5 }}>
             <Trans>Thank you!</Trans>
           </p>
@@ -116,30 +182,6 @@ const Home: NextPage<HomeProps> = ({
     </>
   );
 };
-
-function convertTaxRates(taxRates: TaxRates): Record<City, number> {
-  return {
-    [City.LimsaLominsa]: taxRates['Limsa Lominsa'] ?? 0,
-    [City.Gridania]: taxRates['Gridania'] ?? 0,
-    [City.Uldah]: taxRates["Ul'dah"] ?? 0,
-    [City.Ishgard]: taxRates['Ishgard'] ?? 0,
-    [City.Kugane]: taxRates['Kugane'] ?? 0,
-    [City.Crystarium]: taxRates['Crystarium'] ?? 0,
-    [City.OldSharlayan]: taxRates['Old Sharlayan'] ?? 0,
-  };
-}
-
-function zeroTaxRates(): Record<City, number> {
-  return {
-    [City.LimsaLominsa]: 0,
-    [City.Gridania]: 0,
-    [City.Uldah]: 0,
-    [City.Ishgard]: 0,
-    [City.Kugane]: 0,
-    [City.Crystarium]: 0,
-    [City.OldSharlayan]: 0,
-  };
-}
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   const cookies = new Cookies(ctx.req?.headers.cookie);
@@ -163,70 +205,6 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
     : dcs.find((dc) => dc.worlds.some((w) => w.name.toLowerCase() === world.toLowerCase()))?.name ||
       world;
 
-  let taxes: Record<City, number>;
-  try {
-    const taxRates: TaxRates = await fetch(`${getBaseUrl()}/tax-rates?world=${world}`).then((res) =>
-      res.json()
-    );
-    taxes = convertTaxRates(taxRates);
-  } catch (err) {
-    console.error(err);
-    taxes = zeroTaxRates();
-  }
-
-  let recent: number[] = [];
-  try {
-    const recentlyUpdated = await fetch(`${getBaseUrl()}/extra/stats/recently-updated`).then(
-      (res) => res.json()
-    );
-    recent = recentlyUpdated.items.slice(0, 6);
-  } catch (err) {
-    console.error(err);
-  }
-
-  let leastRecents: number[] = [];
-  try {
-    const leastRecentlyUpdated = await fetch(
-      `${getBaseUrl()}/extra/stats/least-recently-updated?${
-        server.toLowerCase() === world.toLowerCase() ? 'world' : 'dcName'
-      }=${server}`
-    ).then((res) => res.json());
-    leastRecents = leastRecentlyUpdated.items
-      .slice(0, 20)
-      .map((entry: { [x: string]: string | number }) => ({
-        id: entry.itemID,
-        date: entry.lastUploadTime,
-        world: entry.worldName,
-      }));
-  } catch (err) {
-    console.error(err);
-  }
-
-  const dailyUploads: number[] = [];
-  try {
-    const uploadHistory = await fetch(`${getBaseUrl()}/extra/stats/upload-history`).then((res) =>
-      res.json()
-    );
-    dailyUploads.push(...uploadHistory.uploadCountByDay);
-  } catch (err) {
-    console.error(err);
-  }
-
-  const worldUploads: { world: string; count: number }[] = [];
-  try {
-    const worldUploadCounts: Record<string, { count: number; proportion: number }> = await fetch(
-      `${getBaseUrl()}/extra/stats/world-upload-counts`
-    ).then((res) => res.json());
-    worldUploads.push(
-      ...Object.keys(worldUploadCounts).map((k) => ({
-        world: k,
-        count: worldUploadCounts[k].count,
-      }))
-    );
-  } catch (err) {
-    console.error(err);
-  }
-
   const session = await unstable_getServerSession(ctx.req, ctx.res, authOptions);
   const hasSession = !!session;
 
@@ -244,11 +222,6 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
       dcs,
       world,
       server,
-      taxes,
-      recent,
-      leastRecents,
-      dailyUploads,
-      worldUploads,
       hasSession,
       lists,
     },
