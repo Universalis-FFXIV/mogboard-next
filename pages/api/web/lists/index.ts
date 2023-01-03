3a7b545636f829b22033faddc3061e59f6bf8906
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-02 16:36:15 -0800

    Import types only from 'next' This suppresses a warning not to import directly from 'next'.

--- pages/api/web/lists/index.ts
+++ pages/api/web/lists/index.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { Database } from '../../../../db';
 import { PHPObject } from '../../../../db/PHPObject';
 import { unix } from '../../../../db/util';
--- pages/api/web/lodestone/index.ts
+++ pages/api/web/lodestone/index.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { getCharacter, LodestoneCharacter, searchCharacter } from '../../../../service/lodestone';
 import { Database } from '../../../../db';
 import { authOptions } from '../../auth/[...nextauth]';
