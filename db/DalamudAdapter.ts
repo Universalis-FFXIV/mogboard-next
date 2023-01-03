33ca41a5180f8704e302e56955a870a6fe0ca2d2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-02 16:47:38 -0800

    Rewrite all database calls in next-auth adapter

--- db/DalamudAdapter.ts
+++ db/DalamudAdapter.ts
@@ -1,145 +1,118 @@
 import { Adapter, AdapterUser } from 'next-auth/adapters';
-import { User, UserListCustomType } from '../types/universalis/user';
-import { acquireConn, releaseConn } from './connect';
+import { User } from '../types/universalis/user';
+import { Database } from './';
 import { v4 as uuidv4 } from 'uuid';
-import * as db from './user';
-import * as listsDb from './user-list';
 import { unix } from './util';
 import { PHPObject } from './PHPObject';
+import { FavouritesList, RecentlyViewedList } from './user-list';
 
 export default function DalamudAdapter(): Adapter {
   return {
     async createUser(user) {
-      const conn = await acquireConn();
-      try {
-        const id = uuidv4();
-        const username = typeof user.name === 'string' ? user.name : '';
-        const email = typeof user.email === 'string' ? user.email : '';
-        const mogUser: User = {
-          id,
-          added: unix(),
-          lastOnline: 0,
-          isBanned: false,
-          notes: null,
-          sso: '',
-          username,
-          email,
-          avatar: null,
-          admin: false,
-          patron: 0,
-          patronBenefitUser: null,
-          permissions: null,
-          alertsMax: 5,
-          alertsExpiry: 259200,
-          alertsUpdate: false,
-          ssoDiscordId: null,
-          ssoDiscordAvatar: null,
-          ssoDiscordTokenExpires: null,
-          ssoDiscordTokenAccess: null,
-          ssoDiscordTokenRefresh: null,
-          apiPublicKey: null,
-          apiAnalyticsKey: null,
-          apiRateLimit: 0,
-        };
+      const id = uuidv4();
+      const username = typeof user.name === 'string' ? user.name : '';
+      const email = typeof user.email === 'string' ? user.email : '';
+      const mogUser: User = {
+        id,
+        added: unix(),
+        lastOnline: 0,
+        isBanned: false,
+        notes: null,
+        sso: '',
+        username,
+        email,
+        avatar: null,
+        admin: false,
+        patron: 0,
+        patronBenefitUser: null,
+        permissions: null,
+        alertsMax: 5,
+        alertsExpiry: 259200,
+        alertsUpdate: false,
+        ssoDiscordId: null,
+        ssoDiscordAvatar: null,
+        ssoDiscordTokenExpires: null,
+        ssoDiscordTokenAccess: null,
+        ssoDiscordTokenRefresh: null,
+        apiPublicKey: null,
+        apiAnalyticsKey: null,
+        apiRateLimit: 0,
+      };
 
-        await db.createUser(mogUser, conn);
+      await Database.createUser(mogUser);
 
-        // Create the user's custom lists
-        const recentlyViewed = listsDb.RecentlyViewedList(uuidv4(), id, new PHPObject());
-        await listsDb.createUserList(recentlyViewed, conn);
+      // Create the user's custom lists
+      const recentlyViewed = RecentlyViewedList(uuidv4(), id, new PHPObject());
+      await Database.createUserList(recentlyViewed);
 
-        const faves = listsDb.FavouritesList(uuidv4(), id, new PHPObject());
-        await listsDb.createUserList(faves, conn);
+      const faves = FavouritesList(uuidv4(), id, new PHPObject());
+      await Database.createUserList(faves);
 
-        return {
-          id,
-          name: username,
-          email: email,
-          emailVerified: null,
-          image: null,
-        } as AdapterUser;
-      } finally {
-        await releaseConn(conn);
-      }
+      return {
+        id,
+        name: username,
+        email: email,
+        emailVerified: null,
+        image: null,
+      } as AdapterUser;
     },
     async getUser(id) {
-      const conn = await acquireConn();
-      try {
-        const user = await db.getUser(id, conn);
-        if (user == null) {
-          return null;
-        }
-
-        return {
-          id: user.id,
-          name: user.username,
-          email: user.email,
-          emailVerified: null,
-          image: user.avatar || user.ssoDiscordAvatar,
-        };
-      } finally {
-        await releaseConn(conn);
+      const user = await Database.getUser(id);
+      if (user == null) {
+        return null;
       }
+
+      return {
+        id: user.id,
+        name: user.username,
+        email: user.email,
+        emailVerified: null,
+        image: user.avatar || user.ssoDiscordAvatar,
+      };
     },
     async getUserByEmail(email) {
-      const conn = await acquireConn();
-      try {
-        const user = await db.getUserByEmail(email, conn);
-        if (user == null) {
-          return null;
-        }
-
-        return {
-          id: user.id,
-          name: user.username,
-          email: user.email,
-          emailVerified: null,
-          image: user.avatar || user.ssoDiscordAvatar,
-        };
-      } finally {
-        await releaseConn(conn);
+      const user = await Database.getUserByEmail(email);
+      if (user == null) {
+        return null;
       }
+
+      return {
+        id: user.id,
+        name: user.username,
+        email: user.email,
+        emailVerified: null,
+        image: user.avatar || user.ssoDiscordAvatar,
+      };
     },
     async getUserByAccount(account) {
-      const conn = await acquireConn();
-      try {
-        let user: User | null = null;
-        if (account.provider === 'discord') {
-          user = await db.getUserByDiscordId(account.providerAccountId, conn);
-        } else {
-          return null;
-        }
-
-        if (user == null) {
-          return null;
-        }
-
-        return {
-          id: user.id,
-          name: user.username,
-          email: user.email,
-          emailVerified: null,
-          image: user.avatar || user.ssoDiscordAvatar,
-        };
-      } finally {
-        await releaseConn(conn);
+      let user: User | null = null;
+      if (account.provider === 'discord') {
+        user = await Database.getUserByDiscordId(account.providerAccountId);
+      } else {
+        return null;
       }
+
+      if (user == null) {
+        return null;
+      }
+
+      return {
+        id: user.id,
+        name: user.username,
+        email: user.email,
+        emailVerified: null,
+        image: user.avatar || user.ssoDiscordAvatar,
+      };
     },
     async linkAccount(account) {
-      const conn = await acquireConn();
-      try {
-        if (account.provider === 'discord') {
-          await db.updateUserDiscord(
-            account.userId,
-            account.providerAccountId,
-            account.expires_at!,
-            account.access_token!,
-            account.refresh_token!,
-            conn
-          );
-        }
-      } finally {
-        await releaseConn(conn);
+      if (account.provider === 'discord') {
+        await Database.updateUserDiscord(
+          account.userId,
+          account.providerAccountId,
+          account.expires_at!,
+          account.access_token!,
+          account.refresh_token!
+        );
       }
     },
     async createSession() {
@@ -155,47 +128,32 @@ export default function DalamudAdapter(): Adapter {
       throw new Error('Using JWT-based sessions.');
     },
     async updateUser(user) {
-      const conn = await acquireConn();
-      try {
-        const mogUser = await db.getUser(user.id!, conn);
-        if (mogUser == null) {
-          throw new Error('User was not found.');
-        }
-
-        const id = user.id!;
-        const username = user.name ?? mogUser.username;
-        const email = user.email ?? mogUser.email;
-        const avatar = user.image || mogUser.avatar || '';
-
-        await db.updateUserBasic(id, username, email, avatar ?? '', conn);
-
-        return {
-          id,
-          name: username,
-          email,
-          emailVerified: null,
-          image: avatar,
-        };
-      } finally {
-        await releaseConn(conn);
+      const mogUser = await Database.getUser(user.id!);
+      if (mogUser == null) {
+        throw new Error('User was not found.');
       }
+
+      const id = user.id!;
+      const username = user.name ?? mogUser.username;
+      const email = user.email ?? mogUser.email;
+      const avatar = user.image || mogUser.avatar || '';
+
+      await Database.updateUserBasic(id, username, email, avatar ?? '');
+
+      return {
+        id,
+        name: username,
+        email,
+        emailVerified: null,
+        image: avatar,
+      };
     },
     async deleteUser(id) {
-      const conn = await acquireConn();
-      try {
-        await db.deleteUser(id, conn);
-      } finally {
-        await releaseConn(conn);
-      }
+      await Database.deleteUser(id);
     },
     async unlinkAccount(account) {
-      const conn = await acquireConn();
-      try {
-        if (account.provider === 'discord') {
-          await db.removeUserDiscord(account.providerAccountId, conn);
-        }
-      } finally {
-        await releaseConn(conn);
+      if (account.provider === 'discord') {
+        await Database.removeUserDiscord(account.providerAccountId);
       }
     },
   };
