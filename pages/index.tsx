72a60a7f8e78f670137db4451912c7c79176697b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-02 22:04:24 -0800

    Do all data fetching in the client on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import type { GetServerSidePropsContext, NextPage } from 'next';
+import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Cookies } from 'react-cookie';
 import HomeAction from '../components/Home/HomeAction/HomeAction';
 import HomeLoggedOut from '../components/Home/HomeLoggedOut/HomeLoggedOut';
 import HomeNavbar from '../components/Home/HomeNavbar/HomeNavBar';
@@ -12,28 +11,18 @@ import UploadCountPanel from '../components/UploadCountPanel/UploadCountPanel';
 import WorldUploadCountsPanel from '../components/WorldUploadCountsPanel/WorldUploadCountsPanel';
 import { City } from '../types/game/City';
 import { UserList } from '../types/universalis/user';
-import { Database } from '../db';
-import { DataCenter } from '../types/game/DataCenter';
 import HomeUserList from '../components/Home/HomeUserList/HomeUserList';
 import { useState } from 'react';
 import { Trans } from '@lingui/macro';
-import { authOptions } from './api/auth/[...nextauth]';
 import { getItem } from '../data/game';
 import useSettings from '../hooks/useSettings';
 import { getServers } from '../service/servers';
 import { TaxRates } from '../types/universalis/TaxRates';
-import { unstable_getServerSession } from 'next-auth';
 import HomeLeastRecentlyUpdated from '../components/Home/HomeLeastRecentlyUpdated/HomeLeastRecentlyUpdated';
 import { getBaseUrl } from '../service/universalis';
 import useSWR from 'swr';
-
-interface HomeProps {
-  dcs: DataCenter[];
-  world: string;
-  server: string;
-  hasSession: boolean;
-  lists: UserList[];
-}
+import useSWRImmutable from 'swr/immutable';
+import { useSession } from 'next-auth/react';
 
 function sum(arr: number[], start: number, end: number) {
   return arr.slice(start, end).reduce((a, b) => a + b, 0);
@@ -63,11 +52,33 @@ function zeroTaxRates(): Record<City, number> {
   };
 }
 
-const Home: NextPage<HomeProps> = ({ dcs, world, server, hasSession, lists }: HomeProps) => {
+const Home: NextPage = () => {
+  const { status: sessionStatus } = useSession();
+
   const [settings] = useSettings();
   const lang = settings['mogboard_language'] || 'en';
 
   const [selectedList, setSelectedList] = useState<UserList | undefined>();
+
+  const world = settings['mogboard_server'] || 'Phoenix';
+
+  const { data: dcs } = useSWRImmutable('$servers', () =>
+    getServers().then((servers) =>
+      servers.dcs
+        .map((dc) => ({
+          name: dc.name,
+          region: dc.region,
+          worlds: dc.worlds.sort((a, b) => a.name.localeCompare(b.name)),
+        }))
+        .sort((a, b) => a.region.localeCompare(b.region))
+    )
+  );
+
+  const showHomeWorld = settings['mogboard_homeworld'] === 'yes';
+  const server = showHomeWorld
+    ? world
+    : (dcs ?? []).find((dc) => dc.worlds.some((w) => w.name.toLowerCase() === world.toLowerCase()))
+        ?.name || world;
 
   const { data: taxes } = useSWR<Record<City, number>>(
     `${getBaseUrl()}/tax-rates?world=${world}`,
@@ -120,6 +131,10 @@ const Home: NextPage<HomeProps> = ({ dcs, world, server, hasSession, lists }: Ho
         )
   );
 
+  const { data: lists } = useSWR<UserList[]>('/api/web/lists', (url) =>
+    fetch(url).then((res) => res.json())
+  );
+
   const title = 'Universalis';
   const description =
     'Final Fantasy XIV Online: Market Board aggregator. Find Prices, track Item History and create Price Alerts. Anywhere, anytime.';
@@ -135,10 +150,10 @@ const Home: NextPage<HomeProps> = ({ dcs, world, server, hasSession, lists }: Ho
 
       <div className="home">
         <HomeNavbar
-          hasSession={hasSession}
-          lists={lists.sort((a, b) => b.updated - a.updated)}
+          hasSession={sessionStatus === 'authenticated'}
+          lists={(lists ?? []).sort((a, b) => b.updated - a.updated)}
           onListSelected={(id) => {
-            const list = lists.find((list) => list.id === id);
+            const list = (lists ?? []).find((list) => list.id === id);
             if (!selectedList || list?.id !== selectedList?.id) {
               setSelectedList(list);
             } else if (list.id === selectedList.id) {
@@ -148,8 +163,8 @@ const Home: NextPage<HomeProps> = ({ dcs, world, server, hasSession, lists }: Ho
         />
         <div>
           <HomeNews />
-          {selectedList && <HomeUserList dcs={dcs} list={selectedList} />}
-          <LoggedOut hasSession={hasSession}>
+          {selectedList && <HomeUserList dcs={dcs ?? []} list={selectedList} />}
+          <LoggedOut hasSession={sessionStatus === 'authenticated'}>
             <HomeLoggedOut />
           </LoggedOut>
           <HomeLeastRecentlyUpdated
@@ -182,50 +197,5 @@ const Home: NextPage<HomeProps> = ({ dcs, world, server, hasSession, lists }: Ho
     </>
   );
 };
-
-export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-  const cookies = new Cookies(ctx.req?.headers.cookie);
-  const world = cookies.get<string | undefined>('mogboard_server') || 'Phoenix';
-
-  let dcs: DataCenter[] = [];
-  try {
-    const servers = await getServers();
-    dcs = servers.dcs.map((dc) => ({
-      name: dc.name,
-      region: dc.region,
-      worlds: dc.worlds.sort((a, b) => a.name.localeCompare(b.name)),
-    }));
-  } catch (err) {
-    console.error(err);
-  }
-
-  const showHomeWorld = cookies.get<string | undefined>('mogboard_homeworld') === 'yes';
-  const server = showHomeWorld
-    ? world
-    : dcs.find((dc) => dc.worlds.some((w) => w.name.toLowerCase() === world.toLowerCase()))?.name ||
-      world;
-
-  const session = await unstable_getServerSession(ctx.req, ctx.res, authOptions);
-  const hasSession = !!session;
-
-  let lists: UserList[] = [];
-  if (session?.user?.id) {
-    try {
-      lists = await Database.getUserLists(session.user.id);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  return {
-    props: {
-      dcs,
-      world,
-      server,
-      hasSession,
-      lists,
-    },
-  };
-}
 
 export default Home;
