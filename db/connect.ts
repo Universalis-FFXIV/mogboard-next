6c0bfee8da4806cece032d669fcab7fee7933731
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-28 10:23:11 -0800

    Fix connection pooling breaking due to HMR

--- db/connect.ts
+++ db/connect.ts
@@ -4,19 +4,33 @@ const isDev = process.env['APP_ENV'] !== 'prod';
 
 const port = parseInt(process.env['DATABASE_PORT'] ?? '');
 const connections = parseInt(process.env['DATABASE_CONNECTIONS'] ?? '');
-const pool = mariadb.createPool({
-  host: process.env['DATABASE_HOST'] ?? '',
-  port: isNaN(port) ? 0 : port,
-  user: process.env['DATABASE_USER'] ?? '',
-  password: process.env['DATABASE_PASS'] ?? '',
-  database: process.env['DATABASE_NAME'] ?? '',
-  connectionLimit: connections,
-  idleTimeout: 30000,
-  trace: isDev,
-});
+
+declare namespace global {
+  export var pool: mariadb.Pool | undefined;
+}
+
+// In development, hot-reloading rebuilds the pool, so the database
+// hits the connection limit fairly quickly. This hack prevents that
+// from happening.
+if (global.pool == null) {
+  global.pool = mariadb.createPool({
+    host: process.env['DATABASE_HOST'] ?? '',
+    port: isNaN(port) ? 0 : port,
+    user: process.env['DATABASE_USER'] ?? '',
+    password: process.env['DATABASE_PASS'] ?? '',
+    database: process.env['DATABASE_NAME'] ?? '',
+    connectionLimit: connections,
+    idleTimeout: 30000,
+    trace: isDev,
+  });
+}
 
 export function acquireConn() {
-  return pool.getConnection();
+  if (global.pool == null) {
+    throw new Error('The connection pool has not been initialized.');
+  }
+
+  return global.pool.getConnection();
 }
 
 export function releaseConn(conn: mariadb.PoolConnection) {
