c912626eb5120bb8ff659f99626aec849b7a7e1b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2023-01-28 10:23:39 -0800

    Throw error when alert fetch fails

--- hooks/useAlerts.ts
+++ hooks/useAlerts.ts
@@ -4,7 +4,15 @@ import { UserAlert } from '../types/universalis/user';
 export default function useAlerts() {
   return useSWR('/api/web/alerts', (url) =>
     fetch(url)
-      .then((res) => res.json())
+      .then(async (res) => {
+        if (!res.ok) {
+          const body = res.headers.get('Content-Type')?.includes('application/json')
+            ? (await res.json()).message
+            : await res.text();
+          throw new Error(body);
+        }
+        return await res.json();
+      })
       .then((res) => res as UserAlert[])
       .catch(console.error)
   );
