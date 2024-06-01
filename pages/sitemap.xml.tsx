8258765e682e386af20eede6acfcdf420bf80a80
cohenaj194 <cohenaj194@gmail.com>
2024-06-01 15:05:25 -0400

    add sitemap.xml

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,48 @@
+import { GetServerSideProps } from 'next';
+
+const Sitemap = () => {
+  return null;
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const baseUrl = 'https://universalis.app';
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  <url>
+    <loc>${baseUrl}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <priority>1.00</priority>
+  </url>
+  <url>
+    <loc>https://docs.universalis.app</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>https://status.universalis.app</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/about</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <priority>0.80</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/contribute</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <priority>0.80</priority>
+  </url>
+  </urlset>`;
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
